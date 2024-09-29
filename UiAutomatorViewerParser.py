a53ef43f4338a1ba9c17c4dff456c0a866d2862c
Missyouag <asdzxfvgb@sina.com>
2024-09-29 10:17:32 +0800

    修正应该显示内容

--- UiAutomatorViewerParser.py
+++ UiAutomatorViewerParser.py
@@ -12,7 +12,11 @@ import xml.etree.ElementTree as et
 
 from PIL import ImageGrab
 
-from uiautomation.uiautomation import _GetDictKeyName, Control, ExpandCollapsePattern, ExpandCollapseState, GridItemPattern, GridPattern, PatternIdNames, RangeValuePattern, ScrollPattern, SelectionItemPattern, TextPattern, TogglePattern, ToggleState, ValuePattern
+from uiautomation.uiautomation import _GetDictKeyName, Control,  ExpandCollapseState, \
+      PatternIdNames, \
+        RangeValuePattern, ScrollPattern,  TextPattern, TogglePattern, ToggleState, ExpandCollapsePattern,ValuePattern,\
+        GridItemPattern, GridPattern, SelectionItemPattern,ScrollItemPattern,LegacyIAccessiblePattern,InvokePattern,\
+        SelectionPattern,TextEditPattern,TextPattern2
 
 #定义文件存储路径
 tempdir = 'automator'
@@ -42,15 +46,23 @@ def control2Xml(control: Control,processId = 0):
     tree = et.Element('node')
     # 没有class ?
     # tree.set('class', control.ClassName)
-    tree.set('class',  control.ControlTypeName)
-    tree.set('ClassName', control.ClassName)
-    tree.set('AutomationId', control.AutomationId)
-    # tree.set('ControlType', control.ControlTypeName)
-    tree.set('name', control.Name)
+
+    className = control.ControlTypeName
+    # 把Control 名字去掉，对应下面文档
+    # https://learn.microsoft.com/en-us/windows/win32/winauto/uiauto-controlpatternmapping
+    className = className.replace('Control','')
+    tree.set('class',  className)
+    if control.ClassName:
+        tree.set('ClassName', control.ClassName)
+    if control.ClassName:    
+        tree.set('AutomationId', control.AutomationId)
+    tree.set('ControlType', control.ControlTypeName)
+    mName = control.Name
+    # tree.set('name', mName)
     # tree.set('ControlType', str(control.ControlType))
     
     #  兼容 Android UI Autimator Viewer 显示名称
-    tree.set("text","")
+    tree.set("text",mName)
     tree.set("content-desc","")
     tree.set("index","")
     #  兼容 ===================== 显示名称
@@ -65,9 +77,14 @@ def control2Xml(control: Control,processId = 0):
 
 
     supportedPatterns = list(filter(lambda t: t[0], ((control.GetPattern(id_), name) for id_, name in PatternIdNames.items())))
+    # tree.set(f'supportedPatterns',str(len(supportedPatterns)))
+    patternValue = None
     for pt, name in supportedPatterns:
+        name=name.replace("Pattern","")
         if isinstance(pt, ValuePattern) or isinstance(pt, RangeValuePattern):
-            tree.set(f'{name}',str(pt.Value))
+            if pt.Value and patternValue != pt.Value:
+                patternValue = pt.Value
+                tree.set(f'{name}',str(pt.Value))
         elif isinstance(pt, TogglePattern):
             tree.set(f'{name}',str(_GetDictKeyName(ToggleState.__dict__, pt.ToggleState)))
         elif isinstance(pt, SelectionItemPattern):
@@ -75,17 +92,51 @@ def control2Xml(control: Control,processId = 0):
         elif isinstance(pt, ExpandCollapsePattern):
             tree.set(f'{name}',str(_GetDictKeyName(ExpandCollapseState.__dict__, pt.ExpandCollapseState)))
         elif isinstance(pt, ScrollPattern):
-            tree.set(f'{name}_HorizontalScrollPercent',str(pt.HorizontalScrollPercent))
-            tree.set(f'{name}_VerticalScrollPercent',str(pt.VerticalScrollPercent))
+            tree.set(f'{name}_HorizontalPercent',str(pt.HorizontalScrollPercent))
+            tree.set(f'{name}_VerticalPercent',str(pt.VerticalScrollPercent))
+            tree.set(f'{name}_HorizontalSize',str(pt.HorizontalViewSize))
+            tree.set(f'{name}_VerticalSize',str(pt.VerticalViewSize))
         elif isinstance(pt, GridPattern):
             tree.set(f'{name}_RowCount',str(pt.RowCount))
             tree.set(f'{name}_ColumnCount',str(pt.ColumnCount))
         elif isinstance(pt, GridItemPattern):
             tree.set(f'{name}_Row',str(pt.Row))
             tree.set(f'{name}_Column',str(pt.Column))
-        elif isinstance(pt, TextPattern):
+        elif isinstance(pt, TextPattern) :
             tree.set(f'{name}',str(pt.DocumentRange.GetText()))
-
+        elif isinstance(pt, InvokePattern):
+            tree.set(f'Invoke',str(True))
+        elif isinstance(pt, LegacyIAccessiblePattern ):
+            if pt.ChildId:
+                tree.set(f'{name}_ChildId',str(pt.ChildId))
+            if pt.DefaultAction:
+                tree.set(f'{name}_DefaultAction',str(pt.DefaultAction))
+            if pt.Description:
+                tree.set(f'{name}_Description',str(pt.Description))
+            if pt.Help:
+                tree.set(f'{name}_Help',str(pt.Help))
+            if pt.KeyboardShortcut:
+                tree.set(f'{name}_KeyboardShortcut',str(pt.KeyboardShortcut))
+            if pt.Name and mName!=pt.Name:
+                tree.set(f'{name}_Name',str(pt.Name))
+            tree.set(f'{name}_Role',str(pt.Role))
+            tree.set(f'{name}_State',str(pt.State))
+            if pt.Value and patternValue != pt.Value:
+                patternValue = pt.Value
+                tree.set(f'{name}_Value',str(pt.Value))
+        elif isinstance(pt, TextPattern2):
+            # pt.GetActiveComposition()
+            tree.set(f'TextPattern2',str(True))
+        elif isinstance(pt, TextEditPattern):
+            # pt.GetActiveComposition()
+            tree.set(f'TextEdit',str(True))
+        elif isinstance(pt, ScrollItemPattern):
+            tree.set(f'ScrollIntoView',str(True))
+        elif isinstance(pt, SelectionPattern):
+            tree.set(f'CanSelectMultiple',str(pt.CanSelectMultiple))
+            tree.set(f'IsSelectionRequired',str(pt.IsSelectionRequired))
+        else :
+            tree.set(f'supportedPattern_{name}',str(pt))
 
     childrens=control.GetChildren()
     for child in childrens:
